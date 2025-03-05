3b7f9b1c89064aa782346c0e48cd517cfdc15b19
Dhaas-M <94614733+Dhaas-M@users.noreply.github.com>
2025-03-05 14:36:35 +0530

    legacy watch for volumes

---
+++ nodejs-docker/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+const PORT = 3000
+
+app.get("/",(req,res) => {
+    res.status(200).send("Hello World.")
+})
+
+app.listen(process.env.port || PORT, () => {
+    console.log("server started")
+})
